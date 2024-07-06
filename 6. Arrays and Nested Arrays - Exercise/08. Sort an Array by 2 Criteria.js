443885cbfc5ccf05eb6e7f07c05040b7f1e115a4
ffw.georgi.donchev <georgi.donchev@ffw.com>
2024-07-06 16:56:04 +0300

    Adding ex.8, 9, 10 and 11

--- 6. Arrays and Nested Arrays - Exercise/08. Sort an Array by 2 Criteria.js
+++ 6. Arrays and Nested Arrays - Exercise/08. Sort an Array by 2 Criteria.js
@@ -1,5 +1,12 @@
-function sortAnArrayByTwoCriteria(params) {
-  
+function sortAnArrayByTwoCriteria(inputArr) {
+  const output = inputArr
+    .sort( (a, b) => a.length - b.length || a.localeCompare(b) )
+    //   if (a.length !== b.length) {
+    //     return a.length - b.length;
+    //   } else {
+    //     return a.localeCompare(b);
+    .forEach((x) => console.log(x));
 }
 
-sortAnArrayByTwoCriteria();
+sortAnArrayByTwoCriteria(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
+// sortAnArrayByTwoCriteria(["test", "Deny", "omen", "Default"]);
