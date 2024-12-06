be271bd6eced48d80dc195906d03c086d2f4a032
ffw.georgi.donchev <georgi.donchev@ffw.com>
2024-12-06 11:44:35 +0200

    Adding ex. 6 and 7

--- 11. DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ 11. DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    let emailFieldRef = document.querySelector('input#email');
+    
+    emailFieldRef.addEventListener('change', validation)
+    
+    function validation(event) {
+        let pattern = /^[a-z]+@[a-z]+\.[a-z]{2,}$/
+
+        let emailInput = emailFieldRef.value;
+
+        if ( pattern.test(emailInput) ) {
+            emailFieldRef.classList.remove('error')
+        } else {
+            emailFieldRef.classList.add('error')
+        }
+    }
 }
