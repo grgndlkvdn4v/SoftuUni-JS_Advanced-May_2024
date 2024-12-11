6cace7a67f9666e35569215c11584606e650dac9
georgi.donchev.ffw <georgi.donchev@ffw.com>
2024-12-11 11:45:29 +0200

    Adding the exercises from DELL PC

--- 12. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ 12. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,35 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let inputs = document.querySelectorAll('input[type=radio]');
+    let showMoreButtons = document.querySelectorAll('button');
+    let status = '';
+
+    inputs.forEach(input => input.addEventListener('change', locker));
+    showMoreButtons.forEach(button => button.addEventListener('click', showMore));
+
+
+    function locker(event) {
+        let userRadiobuttons = event.currentTarget.parentElement.querySelectorAll('input[type=radio]');
+        let condition = userRadiobuttons[0].checked && !userRadiobuttons[1].checked;
+
+        condition ? status = 'Lock' : status = 'Unlock'
+    }
+
+    function showMore(event) {
+        let hiddenData = event.currentTarget.parentElement.querySelector('div.profile div')
+        let button = event.currentTarget
+
+        if (status === 'Unlock') {
+            switch (button.textContent) {
+                case 'Show more':
+                    hiddenData.style.display = 'inline';
+                    button.textContent = 'Show less';
+                    break;
+                case 'Show less':
+                    hiddenData.style.display = 'none'; 
+                    button.textContent = 'Show more';
+                    break;
+            }
+        }
+                
+    }
 }
