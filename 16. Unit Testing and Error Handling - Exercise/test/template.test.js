a6f4f92093e01438e5773032eb4499f70c5ce974
ffw.georgi.donchev <georgi.donchev@ffw.com>
2024-12-28 14:17:10 +0200

    Adding the template for the unit testing set-up

---
+++ 16. Unit Testing and Error Handling - Exercise/test/template.test.js
@@ -0,0 +1,18 @@
+// npm init 
+// npm install 
+// npm install chai 
+// npm install mocha
+
+// TEMPLATE
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import {  } from "";
+
+describe("", () => {
+
+  it("", () => {
+    expect(  ).to.equal();
+    expect(  ).to.equal();
+  });
+  
+});
